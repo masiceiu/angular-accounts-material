4ad6f7ec2660c8851fd53e7bc7843fbc14c84db2
masiceiu <masiceiu@gmail.com>
2020-02-06 19:51:19 +0600

    2 files modified, 5 added, 6 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from './../service/app.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() { }
+  constructor(appService: AppService) { 
 
+  }
   ngOnInit() {
+
   }
 
 }
